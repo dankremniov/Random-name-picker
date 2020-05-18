e20bc83c609cf304277d35122213bb29a92bd065
Daniel Kremniov <>
2020-05-18 12:09:56 +0100

    MaterialUI integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
 const App = () => {
-  return <div />;
+  return (
+    <>
+      <CssBaseline />
+    </>
+  );
 };
 
 export default App;
